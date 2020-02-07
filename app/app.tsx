42a419616663c8bac18ddad5da7acc827ac09cc3
Harris Robin Kalash <hello@harris.dev>
2020-02-06 22:14:15 -0500

    Initial commit
    Ignite CLI version 3.5.1

---
+++ app/app.tsx
@@ -0,0 +1,145 @@
+// Welcome to the main entry point of the app.
+//
+// In this file, we'll be kicking off our app or storybook.
+
+import "./i18n"
+import React, { useState, useEffect, useRef } from "react"
+import { AppRegistry, YellowBox } from "react-native"
+import { NavigationContainerRef } from "@react-navigation/native"
+import { contains } from "ramda"
+import { enableScreens } from "react-native-screens"
+
+import { RootNavigator, exitRoutes, setRootNavigation } from "./navigation"
+import { useBackButtonHandler } from "./navigation/use-back-button-handler"
+import { RootStore, RootStoreProvider, setupRootStore } from "./models/root-store"
+import * as storage from "./utils/storage"
+import getActiveRouteName from "./navigation/get-active-routename"
+
+// This puts screens in a native ViewController or Activity. If you want fully native
+// stack navigation, use `createNativeStackNavigator` in place of `createStackNavigator`:
+// https://github.com/kmagiera/react-native-screens#using-native-stack-navigator
+enableScreens()
+
+/**
+ * Ignore some yellowbox warnings. Some of these are for deprecated functions
+ * that we haven't gotten around to replacing yet.
+ */
+YellowBox.ignoreWarnings([
+  "componentWillMount is deprecated",
+  "componentWillReceiveProps is deprecated",
+])
+
+/**
+ * Storybook still wants to use ReactNative's AsyncStorage instead of the
+ * react-native-community package; this causes a YellowBox warning. This hack
+ * points RN's AsyncStorage at the community one, fixing the warning. Here's the
+ * Storybook issue about this: https://github.com/storybookjs/storybook/issues/6078
+ */
+const ReactNative = require("react-native")
+Object.defineProperty(ReactNative, "AsyncStorage", {
+  get(): any {
+    return require("@react-native-community/async-storage").default
+  },
+})
+
+/**
+ * Are we allowed to exit the app?  This is called when the back button
+ * is pressed on android.
+ *
+ * @param routeName The currently active route name.
+ */
+const canExit = (routeName: string) => contains(routeName, exitRoutes)
+
+export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE"
+
+/**
+ * This is the root component of our app.
+ */
+export const App: React.FunctionComponent<{}> = () => {
+  const navigationRef = useRef<NavigationContainerRef>()
+  const [rootStore, setRootStore] = useState<RootStore | undefined>(undefined)
+  const [initialNavigationState, setInitialNavigationState] = useState()
+  const [isRestoringNavigationState, setIsRestoringNavigationState] = useState(true)
+
+  setRootNavigation(navigationRef)
+  useBackButtonHandler(navigationRef, canExit)
+
+  /**
+   * Keep track of state changes
+   * Track Screens
+   * Persist State
+   */
+  const routeNameRef = useRef()
+  const onNavigationStateChange = state => {
+    const currentRouteName = getActiveRouteName(state)
+
+    // Save the current route name for later comparision
+    routeNameRef.current = currentRouteName
+
+    // Persist state to storage
+    storage.save(NAVIGATION_PERSISTENCE_KEY, state)
+  }
+
+  useEffect(() => {
+    setupRootStore().then(setRootStore)
+  }, [])
+
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const state = await storage.load(NAVIGATION_PERSISTENCE_KEY)
+
+        if (state) {
+          setInitialNavigationState(state)
+        }
+      } finally {
+        setIsRestoringNavigationState(false)
+      }
+    }
+
+    if (isRestoringNavigationState) {
+      restoreState()
+    }
+  }, [isRestoringNavigationState])
+
+  // Before we show the app, we have to wait for our state to be ready.
+  // In the meantime, don't render anything. This will be the background
+  // color set in native by rootView's background color.
+  //
+  // This step should be completely covered over by the splash screen though.
+  //
+  // You're welcome to swap in your own component to render if your boot up
+  // sequence is too slow though.
+  if (!rootStore) {
+    return null
+  }
+
+  // otherwise, we're ready to render the app
+  return (
+    <RootStoreProvider value={rootStore}>
+      <RootNavigator
+        ref={navigationRef}
+        initialState={initialNavigationState}
+        onStateChange={onNavigationStateChange}
+      />
+    </RootStoreProvider>
+  )
+}
+
+/**
+ * This needs to match what's found in your app_delegate.m and MainActivity.java.
+ */
+const APP_NAME = "bowsernavigationv5"
+
+// Should we show storybook instead of our app?
+//
+// ⚠️ Leave this as `false` when checking into git.
+const SHOW_STORYBOOK = false
+
+let RootComponent = App
+if (__DEV__ && SHOW_STORYBOOK) {
+  // Only include Storybook if we're in dev mode
+  const { StorybookUIRoot } = require("../storybook")
+  RootComponent = StorybookUIRoot
+}
+AppRegistry.registerComponent(APP_NAME, () => RootComponent)
